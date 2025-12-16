15c6327ef1d7345820dd895783a3c91fe657fac2
shzxzzz <wisssemysticaltreeee@gmail.com>
2025-12-16 17:39:05 +0300

    demo commit 4

--- src/pages/Cabinet.jsx
+++ src/pages/Cabinet.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Icon } from '../components/Icons.jsx';
-import cabinetApi  from '../api/cabinetApi';
+import cabinetApi  from '../api/cabinetAPI';
 
 const CabinetPage = () => {
     // ссостояния для данных
